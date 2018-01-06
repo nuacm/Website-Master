ffbae55e1307c84b2a61867dd1d1318acc129d22
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-01-06 11:01:40 -0500

    Swapping source for nuacm logo

--- app/helpers/external_links_helper.rb
+++ app/helpers/external_links_helper.rb
@@ -14,4 +14,8 @@ module ExternalLinksHelper
   def email_redirect
     "mailto:&#116;o&#58;&#37;61&#99;m&#64;cc&#115;&#46;&#110;&#37;&#54;&#53;u&#46;e&#100;u"
   end
+
+  def nuacm_logo_url
+    "https://image.ibb.co/nfWVNG/logo.png"
+  end
 end
